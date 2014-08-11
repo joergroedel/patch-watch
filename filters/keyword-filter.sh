6142761e714c0d8bd70863da75fc607fa1f018f0
Joerg Roedel <jroedel@suse.de>
2014-08-11 14:53:11 +0200

    Add generic keyword filter to unify existing filters
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

---
+++ filters/keyword-filter.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+
+GIT="/usr/bin/git"
+commit=$1
+
+function match_keyword {
+	match=$1
+	tag=$2
+
+	$GIT show -s $commit | grep -i "$match" > /dev/null 2>&1
+	if [ "$?" == "0" ]; then
+		echo $tag
+	fi
+}
+
+match_keyword "fix" "fix"
+match_keyword "Cc: stable@vger.kernel.org" "stable"
+
+exit 0
