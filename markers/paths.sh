2e488cb6514ea1c8a352d67ea95492430126ed05
Joerg Roedel <jroedel@suse.de>
2014-08-23 22:12:51 +0200

    Print full path names of patched files
    Fix that it only gave the file name without the path.
    
    Signed-off-by: Joerg Roedel <jroedel@suse.de>

--- markers/paths.sh
+++ markers/paths.sh
@@ -5,6 +5,6 @@ GIT="/usr/bin/git"
 DIFFSTAT="/usr/bin/diffstat"
 commit=$1
 
-for path in `$GIT show -p $commit | diffstat -l`; do
+for path in `$GIT show -p $commit | diffstat -p1 -l`; do
 	echo "PATH:$path"
 done
